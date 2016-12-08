622e28a5702cb47e1a62e263f1c79229a9501fcb
Steve Kinney <hello@stevekinney.net>
2016-12-08 15:32:04 -0700

    Use Node's require in adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,5 @@
 import DS from 'ember-data';
-// require('electron');
-import database from './database';
-
-// global.database = database;
+const database = requireNode('../database');
 
 export default DS.Adapter.extend({
 
